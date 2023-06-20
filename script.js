81efccb4fc23c22a2dc8da0cee64ed39e4a90a3a
Miguel Vega <vega@Miguels-MacBook-Pro.local>
2023-06-20 17:27:10 -0400

    added more options to make passwords

--- script.js
+++ script.js
@@ -12,18 +12,37 @@ generateBtn.addEventListener("click", writePassword);
 
 // Function to generate a random password
 function generatePassword() {
-  var length = parseInt(prompt("Enter the desired length of the password:"));
-  if (isNaN(length) || length < 8 || length > 128) {
-    alert("Invalid password length. Please enter a number between 8 and 128.");
-    return "";
+    var length = parseInt(prompt("Enter the desired length of the password:"));
+    if (isNaN(length) || length < 8 || length > 128) {
+      alert("Invalid password length. Please enter a number between 8 and 128.");
+      return "";
+    }
+  
+    var includeLowercase = document.getElementById('include-lowercase').checked;
+    var includeUppercase = document.getElementById('include-uppercase').checked;
+    var includeNumbers = document.getElementById('include-numbers').checked;
+    var includeSpecial = document.getElementById('include-special').checked;
+  
+    var characters = '';
+    if (includeLowercase) {
+      characters += 'abcdefghijklmnopqrstuvwxyz';
+    }
+    if (includeUppercase) {
+      characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+    if (includeNumbers) {
+      characters += '0123456789';
+    }
+    if (includeSpecial) {
+      characters += '!@#$%^&*()-_=+[{]}\\|;:\'",<.>/?';
+    }
+  
+    var password = "";
+    for (var i = 0; i < length; i++) {
+      var randomIndex = Math.floor(Math.random() * characters.length);
+      password += characters[randomIndex];
+    }
+  
+    return password;
   }
-
-  var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?";
-  var password = "";
-  for (var i = 0; i < length; i++) {
-    var randomIndex = Math.floor(Math.random() * characters.length);
-    password += characters[randomIndex];
-  }
-
-  return password;
-}
+  
